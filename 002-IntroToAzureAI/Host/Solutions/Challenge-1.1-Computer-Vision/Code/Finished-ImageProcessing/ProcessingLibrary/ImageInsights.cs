1d08bb34eacdbdd73eeaae65966eccd50c56e136
Gino Filicetti <gino@filicetti.net>
2018-09-21 00:28:41 -0400

    Challenge 1.1 - Host - Initial Commit

---
+++ 002-IntroToAzureAI/Host/Solutions/Challenge-1.1-Computer-Vision/Code/Finished-ImageProcessing/ProcessingLibrary/ImageInsights.cs
@@ -0,0 +1,10 @@
+﻿namespace ProcessingLibrary
+{
+    public class ImageInsights
+    {
+        public string ImageId { get; set; }
+        public string Caption { get; set; }
+        public string[] Tags { get; set; }
+
+    }
+}
