9a10c01d7ac41e723ae1b3e8112acc5eb7261a47
Gino Filicetti <gino@filicetti.net>
2018-10-01 15:05:45 -0400

    Updated script and template and should work now

--- 001-IntroToKubernetes/Student/Resources/Templates/build-machine/docker-build-machine-config.sh
+++ 001-IntroToKubernetes/Student/Resources/Templates/build-machine/docker-build-machine-config.sh
@@ -4,38 +4,39 @@
 
 # The first parameter passed in is the user we're going to add
 # to the docker group, if one isn't passed in, use the current user
-#CURUSER = ${1:-$USER}
-CURUSER = ${1:-wthadmin}
-CURUSERHOME = $(eval echo ~$(echo $CURUSER))
+CURUSER=${1:-$USER}
+#CURUSER=${1:-wthadmin}
+CURUSERHOME=$(eval echo ~$(echo $CURUSER))
+echo "Current user is: $CURUSER"
+echo "Current user's home dir is: $CURUSERHOME"
 
 #1. Update the Ubuntu packages and install curl and support for repositories over HTTPS
-#in a single step by typing the following in a single line command. 
-#When asked if you would like to proceed, respond by typing “y” and pressing enter. 
-sudo apt-get -y update && sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
+#in a single step by typing the following in a single line command.
+#When asked if you would like to proceed, respond by typing “y” and pressing enter.
+sudo apt-get -qy update && sudo apt -qy install apt-transport-https ca-certificates curl software-properties-common
 
 #2. Add Docker’s official GPG key by typing the following in a single line command
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 #3. Add Docker’s stable repository to Ubuntu packages list by typing the following in a single line command
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 #4. Update the Ubuntu packages and install Docker engine, node.js and the node package manager
-#in a single step by typing the following in a single line command. 
+#in a single step by typing the following in a single line command.
 #When asked if you would like to proceed, respond by typing “y” and pressing enter.
-sudo apt-get -y update && sudo apt -y install docker-ce nodejs npm
+sudo apt-get -qy update && sudo apt -qy install docker-ce nodejs npm wget
 
-#5.	Now, upgrade the Ubuntu packages to the latest version by typing the following in a single line command. 
+#5.     Now, upgrade the Ubuntu packages to the latest version by typing the following in a single line command.
 #When asked if you would like to proceed, respond by typing “y” and pressing enter.
-sudo apt-get -y upgrade
+sudo apt-get -qy upgrade
 
-#6.	Add your user to the Docker group so that you do not have to elevate privileges with sudo for every command. 
+#6.     Add your user to the Docker group so that you do not have to elevate privileges with sudo for every command.
 sudo usermod -aG docker $CURUSER
 
 #Download and stage source code for the V1 & V2 of the FabMedical application.
 
 #Download FabMedical V1 source code:
-#TODO - change URL to our repo location!!!
-curl -L -o FabMedical.v1.tgz http://aka.ms/FabMedical.v1
+curl -sL -o FabMedical.v1.tgz http://aka.ms/FabMedical.v1
 
 #Create new V1 directory
 mkdir $CURUSERHOME/FabMedical.v1
@@ -44,12 +45,10 @@ mkdir $CURUSERHOME/FabMedical.v1
 tar -C $CURUSERHOME/FabMedical.v1 -xzf FabMedical.v1.tgz
 
 #Download FabMedical V1 source code:
-#TODO - change URL to our repo location AND download V2!!!
-curl -L -o FabMedical.v2.tgz http://aka.ms/FabMedical.v2
+curl -sL -o FabMedical.v2.tgz http://aka.ms/FabMedical.v2
 
 #Create new V1 directory
 mkdir $CURUSERHOME/FabMedical.v2
 
 #Unpack the archive into the new folder
 tar -C $CURUSERHOME/FabMedical.v2 -xzf FabMedical.v2.tgz
-
