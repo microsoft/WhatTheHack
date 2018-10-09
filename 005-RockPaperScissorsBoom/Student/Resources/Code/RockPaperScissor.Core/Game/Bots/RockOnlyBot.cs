a740a03fa6e0434ba72c04df34c4b2eede499d7d
Gino Filicetti <gino@filicetti.net>
2018-10-09 18:34:22 -0400

    First commit of Mike Ricther's hack.

---
+++ 005-RockPaperScissorsBoom/Student/Resources/Code/RockPaperScissor.Core/Game/Bots/RockOnlyBot.cs
@@ -0,0 +1,9 @@
+﻿using RockPaperScissor.Core.Game.Results;
+
+namespace RockPaperScissor.Core.Game.Bots
+{
+    public class RockOnlyBot : BaseBot
+    { 
+        public override Decision GetDecision(PreviousDecisionResult previousResult) => Decision.Rock;
+    }
+}
