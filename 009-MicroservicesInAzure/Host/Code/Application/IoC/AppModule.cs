24be89adf7141d87c9302cb74df0436dbd1e7b7b
Andy Wahrenberger <andywahr@microsoft.com>
2019-02-01 19:34:49 -0600

    Added the Microservices in Azure Hack

---
+++ 009-MicroservicesInAzure/Host/Code/Application/IoC/AppModule.cs
@@ -0,0 +1,17 @@
+﻿using Autofac;
+using ContosoTravel.Web.Application.Interfaces.MVC;
+using ContosoTravel.Web.Application.Services;
+
+namespace ContosoTravel.Web.Application.IoC
+{
+    public class AppModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {  
+            builder.RegisterType<AzureManagement>()
+                   .AsSelf()
+                   .SingleInstance();
+
+        }
+    }
+}
