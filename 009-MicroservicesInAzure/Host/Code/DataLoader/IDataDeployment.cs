a2e4f43ad21ab5e386c501cdb9cbfbf815f55e98
Andy Wahrenberger <andywahr@microsoft.com>
2019-02-01 19:34:49 -0600

    Added the Microservices in Azure Hack

---
+++ 009-MicroservicesInAzure/Host/Code/DataLoader/IDataDeployment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DataLoader
+{
+    public interface IDataDeployment
+    {
+        Task Configure(CancellationToken cancellationToken);
+    }
+}
