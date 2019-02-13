8eab2572f494dc3017662305c170f586a0f8adc6
Mike Richter <mrichter@microsoft.com>
2019-02-12 17:12:40 -0800

    Added Tournament instructions

--- 005-RockPaperScissorsBoom/Student/Resources/Code/RockPaperScissorsBoom.ExampleBot/Program.cs
+++ 005-RockPaperScissorsBoom/Student/Resources/Code/RockPaperScissorsBoom.ExampleBot/Program.cs
@@ -12,7 +12,6 @@ namespace RockPaperScissorsBoom.ExampleBot
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseUrls("http://localhost:5050")
                 .UseStartup<Startup>();
     }
 }
