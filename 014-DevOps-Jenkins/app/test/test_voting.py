7281139b48f218ad59076e7e6b14da93e3c494aa
Kevin Hillinger <kevin.hillinger@microsoft.com>
2019-05-28 11:16:44 -0400

    scaffolding to setup unittest with auto-discovery

---
+++ 014-DevOps-Jenkins/app/test/test_voting.py
@@ -0,0 +1,12 @@
+import sys
+import unittest
+
+class TestVoting(unittest.TestCase):
+  def setUp(self):
+    # setup
+
+    def test_vote1_value_set_from_config(self):
+      self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
