65245e5a78707bc92d7533a1df38e186f1b1491c
Pete Rodriguez <pedrorod@microsoft.com>
2019-07-02 16:49:37 -0500

    Fixed missing equals sign

--- 012-InfraAsCode-Terraform/Host/Solutions/Challenge 2/Challenge 2 - vnet.tf
+++ 012-InfraAsCode-Terraform/Host/Solutions/Challenge 2/Challenge 2 - vnet.tf
@@ -15,7 +15,7 @@ resource "azurerm_virtual_network" "myterraformnetwork" {
     location            = "eastus"
     resource_group_name = "${azurerm_resource_group.rg.name}"
 
-    tags {
+    tags = {
         environment = "WTH Terraform"
     }
 }
