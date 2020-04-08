ab88e3895773120b8f1123f6e316a7e0111225b3
Ali Hussain <alhussai@Alis-MacBook-Pro.local>
2020-04-08 10:53:32 -0700

    add

--- 014-OSS DevOps/Host/Solutions/challenge09/app/app.py
+++ 014-OSS DevOps/Host/Solutions/challenge09/app/app.py
@@ -1,30 +1,43 @@
+import time
+import random
+
 from flask import Flask, request, render_template
-import socket
-import sys
-import os
+from prometheus_flask_exporter import PrometheusMetrics
+
 
 app = Flask(__name__)
+PrometheusMetrics(app)
+
+endpoints = ('test', 'error')
 
 # Load configurations
 app.config.from_pyfile('config_file.cfg')
 
-button1 =   app.config['VOTE1VALUE']  
-button2 =  app.config['VOTE2VALUE']
-title        =  app.config['TITLE']
+button1 = app.config['VOTE1VALUE']
+button2 = app.config['VOTE2VALUE']
+title = app.config['TITLE']
 
 # Change title to host name to demo NLB
 if app.config['SHOWHOST'] == "true":
     title = socket.gethostname()
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route('/test', methods=['GET', 'POST'])
 def index():
     # Vote tracking
     vote1 = 0
     vote2 = 0
 
-    if request.method == 'GET':        
+    if request.method == 'GET':
         # Return index with values
         return render_template("index.html", value1=vote1, value2=vote2, button1=button1, button2=button2, title=title)
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, port=80)
+
+@app.route('/error')
+def oops():
+    return ':(', 500
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run('0.0.0.0', 5000, threaded=True)
