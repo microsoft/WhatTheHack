7a2a1dac266fde9f1c6fc44c5692d207296d8091
David Hoerster <DavidHoerster@users.noreply.github.com>
2021-02-21 21:21:32 -0500

    rearrange student resources and add coach solutions (#35)
    Co-authored-by: David Hoerster <dhoerster@gmail.com>

---
+++ 021-IdentityForApps/Coach/Solutions/Apps/verify/Constants.cs
@@ -0,0 +1,19 @@
+
+namespace Verify_inator
+{
+    public static class Constants
+    {
+        public static class ClaimTypes
+        {
+            public const string ObjectId = "oid";
+        }
+
+        public static class UserAttributes
+        {
+            public const string ConsultantID = nameof(ConsultantID);
+            public const string TerritoryName = nameof(TerritoryName);
+        }
+
+    }
+
+}
