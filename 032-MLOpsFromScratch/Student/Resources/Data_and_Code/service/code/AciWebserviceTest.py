8308d0c652cba70c9631765c5eb78742f2d1fc00
Shiva Chittamuru <shchitt@microsoft.com>
2021-02-24 11:54:04 -0800

    'updates'

---
+++ 032-MLOpsFromScratch/Student/Resources/Data_and_Code/service/code/AciWebserviceTest.py
@@ -0,0 +1,90 @@
+"""
+Copyright (C) Microsoft Corporation. All rights reserved.​
+ ​
+Microsoft Corporation (“Microsoft”) grants you a nonexclusive, perpetual,
+royalty-free right to use, copy, and modify the software code provided by us
+("Software Code"). You may not sublicense the Software Code or any use of it
+(except to your affiliates and to vendors to perform work on your behalf)
+through distribution, network access, service agreement, lease, rental, or
+otherwise. This license does not purport to express any claim of ownership over
+data you may have shared with Microsoft in the creation of the Software Code.
+Unless applicable law gives you more rights, Microsoft reserves all other
+rights not expressly granted herein, whether by implication, estoppel or
+otherwise. ​
+ ​
+THE SOFTWARE CODE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+MICROSOFT OR ITS LICENSORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THE SOFTWARE CODE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+"""
+import numpy
+import os
+import json
+import datetime
+import sys
+from operator import attrgetter
+from azureml.core import Workspace
+from azureml.core.model import Model
+from azureml.core.image import Image
+from azureml.core.webservice import Webservice
+from azureml.core.webservice import AciWebservice
+from azureml.core.authentication import AzureCliAuthentication
+
+
+root_path = os.path.abspath(os.path.join(__file__, "../../.."))
+config_path = os.path.join(root_path, 'configuration/config.json')
+with open(config_path) as f:
+    config = json.load(f)
+
+workspace_name = config["workspace_name"]
+resource_group = config["resource_group"]
+subscription_id = config["subscription_id"]
+location = config["location"]
+
+cli_auth = AzureCliAuthentication()
+
+
+# Get workspace
+#ws = Workspace.from_config(auth=cli_auth)
+ws = Workspace.get(
+    name=workspace_name,
+    subscription_id=subscription_id,
+    resource_group=resource_group,
+    auth=cli_auth
+)
+
+
+# Get workspace
+#ws = Workspace.from_config(auth=cli_auth)
+# Get the ACI Details
+try:
+    wci_path = os.path.join(root_path, 'configuration/aci_webservice.json')
+    with open(wci_path) as f:
+        config = json.load(f)
+except:
+    print("No new model, thus no deployment on ACI")
+    # raise Exception('No new model to register as production model perform better')
+    sys.exit(0)
+
+service_name = config["aci_name"]
+# Get the hosted web service
+service = Webservice(name=service_name, workspace=ws)
+
+# Input for Model with all features
+step_size = [3]
+test_sample = json.dumps({"data": step_size})
+test_sample = bytes(test_sample, encoding="utf8")
+print(step_size)
+try:
+    prediction = service.run(input_data=test_sample)
+    print(prediction)
+except Exception as e:
+    result = str(e)
+    print(result)
+    raise Exception("ACI service is not working as expected")
