26cd224743ef36e0fb5110e72b86f9cf91ccd337
Pete Rodriguez <perktime@users.noreply.github.com>
2021-03-20 08:41:33 -0500

    Renamed a file. Removed PAR from parameters.json

---
+++ 033-OSSDatabaseMigration/Student/Resources/HelmCharts/ContosoPizza/uninstall-pizza.sh
@@ -0,0 +1,5 @@
+helm uninstall wth-postgresql
+helm uninstall wth-mysql
+helm uninstall mysql-contosopizza
+helm uninstall postgres-contosopizza
+echo "Use kubectl get ns to make sure your pods are not in a Terminating status before redeploying"
