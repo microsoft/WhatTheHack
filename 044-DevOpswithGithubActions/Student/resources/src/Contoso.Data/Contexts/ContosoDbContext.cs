18f65cde19776f8f6e44b074bce4a656f1358b9e
Vijay Jethani <abc@org.com>
2021-07-12 19:24:57 +0530

    Code Update

---
+++ 044-DevOpswithGithubActions/Student/resources/src/Contoso.Data/Contexts/ContosoDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Contoso.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+namespace Contoso.Data.Contexts
+{
+    public class ContosoDbContext: DbContext
+    {
+        public ContosoDbContext(DbContextOptions<ContosoDbContext> options): base(options)
+        {
+        }
+
+        // Entities
+        public DbSet<Dependent> Dependents { get; set; }
+        public DbSet<Person> People { get; set; }
+        public DbSet<Policy> Policies { get; set; }
+        public DbSet<PolicyHolder> PolicyHolders { get; set; }
+    }
+}
