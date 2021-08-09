75f9fd1af50007880ba7ccf238049574309efd40
Jason Masten <jamasten@microsoft.com>
2021-08-09 00:04:52 -0400

    removed erroraction on registry settings

--- 007-AzureMonitoring/Student/Resources/scripts/SetupVSServer.ps1
+++ 007-AzureMonitoring/Student/Resources/scripts/SetupVSServer.ps1
@@ -37,11 +37,10 @@ try
     # Disable Internet Explorer Enhanced Security Configuration
     $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
     $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
-    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0 -Force -ErrorAction 'Stop'
-    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0 -Force -ErrorAction 'Stop'
+    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0 -Force
+    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0 -Force
     Stop-Process -Name 'Explorer' -Force -ErrorAction 'Stop'
     Write-Log -Message 'Disabled IE Enhanced Security' -Type 'INFO'
-
 
     # Download eShopOnWeb to c:\eShopOnWeb and extract contents
     $zipFileeShopTemp = [System.IO.Path]::GetTempPath().ToString() + "eShopOnWeb-master.zip"
