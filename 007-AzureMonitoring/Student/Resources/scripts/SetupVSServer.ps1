73fcd46ccb84decc85832a9b800f41f7495f0ee4
Jason Masten <jamasten@microsoft.com>
2021-08-09 01:58:04 -0400

    removed error for stopping explorer

--- 007-AzureMonitoring/Student/Resources/scripts/SetupVSServer.ps1
+++ 007-AzureMonitoring/Student/Resources/scripts/SetupVSServer.ps1
@@ -40,7 +40,7 @@ try
     $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
     Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0 -Force
     Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0 -Force
-    Stop-Process -Name 'Explorer' -Force -ErrorAction 'Stop'
+    Stop-Process -Name 'Explorer' -Force
     Write-Log -Message 'Disabled IE Enhanced Security' -Type 'INFO'
 
     # Download eShopOnWeb to c:\eShopOnWeb and extract contents
