1a67a0fffe53a30d5bbeaca3e80fc473875616cf
jethanivijay <vijayjethani@yahoo.com>
2021-09-01 19:57:57 +0530

    Update

--- 044-DevOpswithGithubActions/Student/resources/src/Contoso.Azure/KeyVault/KeyVaultConfig.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Contoso.Azure.KeyVault
-{
-    public static class KeyVaultConfig
-    {
-        public static string GetKeyVaultEndpoint(string vaultName) => $"https://{vaultName.Trim().ToLower()}.vault.azure.net/";
-    }
-}
