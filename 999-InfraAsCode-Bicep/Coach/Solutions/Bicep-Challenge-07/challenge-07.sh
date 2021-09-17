209f5015b477514ba0946329a684bff46a617b44
Whowong <ahuang05@hotmail.com>
2021-09-17 16:52:59 -0700

    Moving the Bicep Solutions to match the challenge updates

--- 999-InfraAsCode-Bicep/Coach/Solutions/Bicep-Challenge-07/challenge-07.sh
+++
@@ -1,7 +0,0 @@
-LOCATION='eastus'
-DEPLOYMENT_NAME='challenge-07-deployment'
-
-az deployment sub create \
-	--name $DEPLOYMENT_NAME \
-    --template-file challenge-07.bicep \
-	--parameters challenge-07.parameters.json \
