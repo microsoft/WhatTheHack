1e8a46c59446757213b9bc8383365229fd41c429
Pete Rodriguez <perktime@users.noreply.github.com>
2022-01-10 15:30:35 -0600

    More changes for Oracle

--- 033-OSSDatabaseMigration/Student/Resources/HelmCharts/ContosoPizza/deploy-pizza.sh
+++ 033-OSSDatabaseMigration/Student/Resources/HelmCharts/ContosoPizza/deploy-pizza.sh
@@ -12,6 +12,8 @@ for ((i = 0 ; i < 30 ; i++)); do
 
     if [ "$pgStatus" != "$status" ]; then
         sleep 10
+    else   
+        break
     fi
 done
 
@@ -30,6 +32,8 @@ for ((i = 0 ; i < 30 ; i++)); do
 
     if [ "$mysqlStatus" != "$status" ]; then
         sleep 30
+    else
+        break
     fi
 done
 
@@ -37,8 +41,8 @@ done
 
 kubectl -n mysql exec deploy/mysql -it -- /usr/bin/mysql -u root -pOCPHack8 <./mysql.sql
 
-postgresClusterIP=$(kubectl -n postgresql get svc -o json |jq .items[0].spec.clusterIP |tr -d '"')
-mysqlClusterIP=$(kubectl -n mysql get svc -o json |jq .items[0].spec.clusterIP |tr -d '"')
+postgresClusterIP=$(kubectl -n postgresql get svc -o jsonpath="{.items[0].spec.clusterIP}")
+mysqlClusterIP=$(kubectl -n mysql get svc -o jsonpath="{.items[0].spec.clusterIP}")
 
 sed "s/XXX.XXX.XXX.XXX/$postgresClusterIP/" ./values-postgresql-orig.yaml >temp_postgresql.yaml && mv temp_postgresql.yaml ./values-postgresql.yaml
 sed "s/XXX.XXX.XXX.XXX/$mysqlClusterIP/" ./values-mysql-orig.yaml >temp_mysql.yaml && mv temp_mysql.yaml ./values-mysql.yaml
@@ -48,23 +52,29 @@ helm upgrade --install mysql-contosopizza . -f ./values.yaml -f ./values-mysql.y
 helm upgrade --install postgres-contosopizza . -f ./values.yaml -f ./values-postgresql.yaml
 
 for ((i = 0 ; i < 30 ; i++)); do
-    appStatus=$(kubectl -n contosoappmysql get svc -o json  |jq .items[0].status.loadBalancer.ingress[0].ip |tr -d '"')   
-
-    if [ "$appStatus" == "null" ]; then
+    appStatus=$(kubectl -n contosoappmysql get svc -o jsonpath="{.items[0].status.loadBalancer.ingress[0].ip}")
+    if [[ $appStatus =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+        echo $appStatus
+        break
+    else
+        echo "Waiting for load balancer IP address for contosoappmysql"
         sleep 30
     fi
 done
 
 for ((i = 0 ; i < 30 ; i++)); do
-    appStatus=$(kubectl -n contosoappmysql get svc -o json  |jq .items[0].status.loadBalancer.ingress[0].ip |tr -d '"')   
-
-    if [ "$appStatus" == "null" ]; then
+    appStatus=$(kubectl -n contosoapppostgres get svc -o jsonpath="{.items[0].status.loadBalancer.ingress[0].ip}")
+    if [[ $appStatus =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+        echo $appStatus
+        break
+    else
+        echo "Waiting for load balancer IP address for contosoapppostgres"
         sleep 30
     fi
 done
 
-postgresAppIP=$(kubectl -n contosoapppostgres get svc -o json  |jq .items[0].status.loadBalancer.ingress[0].ip|tr -d '"')
-mysqlAppIP=$(kubectl -n contosoappmysql get svc -o json  |jq .items[0].status.loadBalancer.ingress[0].ip |tr -d '"')
+postgresAppIP=$(kubectl -n contosoapppostgres get svc -o jsonpath="{.items[0].status.loadBalancer.ingress[0].ip}")
+mysqlAppIP=$(kubectl -n contosoappmysql get svc -o jsonpath="{.items[0].status.loadBalancer.ingress[0].ip}")
 
 echo "Pizzeria app on MySQL is ready at http://$mysqlAppIP:8081/pizzeria"
 echo "Pizzeria app on PostgreSQL is ready at http://$postgresAppIP:8082/pizzeria"
