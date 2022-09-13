af05b7763dc6d5c71f28e71fba9dea7c52598d36
Jerry@MSFT <jerryemis@me.com>
2022-09-13 15:44:56 -0400

    Add files via upload

---
+++ xxx-ChaosStudio4AKS/Student/Resources/WestUS-AKS/ARM-Templates/KubernetesCluster/create-cluster.sh
@@ -0,0 +1,32 @@
+# Add line to set login to az
+#az login
+# Set your azure subscription
+#az account set -s "<subscription-id>"
+# Defines the ARM template file location
+export templateFile="aks-cluster.json"
+
+# Defines the parameters that will be used in the ARM template
+export parameterFile="parameters.json"
+
+# Defines the name of the Resource Group our resources are deployed into
+export resourceGroupName="PizzaAppWest"
+
+export clusterName="pizzaappwest"
+
+export location="westus"
+
+# Creates the resources group if it does not already exist
+az group create --name $resourceGroupName --location $location
+
+# Creates the Kubernetes cluster and the associated resources and dependencies for the cluster
+az deployment group create --name dataProductionDeployment --resource-group $resourceGroupName --template-file $templateFile --parameters $parameterFile 
+
+# Install the Kubectl CLI. This will be used to interact with the remote Kubernetes cluster
+#sudo az aks install-cli
+
+# Get the Credentials to Access the Cluster with Kubectl
+az aks get-credentials --name $clusterName --resource-group $resourceGroupName
+
+# List the node pools - expect two aks nodepools
+
+az aks nodepool list --resource-group $resourceGroupName --cluster-name $clusterName -o table
