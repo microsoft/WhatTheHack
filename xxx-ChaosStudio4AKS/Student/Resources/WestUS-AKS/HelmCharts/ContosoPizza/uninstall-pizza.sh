4bf652b65e003a373371cef1ec2cff3d1254c4d9
Jerry@MSFT <jerryemis@me.com>
2022-09-13 14:46:17 -0500

    Delete xxx-ChaosStudio4AKS/Student/Resources/WestUS-AKS directory

--- xxx-ChaosStudio4AKS/Student/Resources/WestUS-AKS/HelmCharts/ContosoPizza/uninstall-pizza.sh
+++
@@ -1,6 +0,0 @@
-helm uninstall wth-postgresql
-helm uninstall wth-mysql
-helm uninstall mysql-contosopizza
-helm uninstall postgres-contosopizza
-echo ""
-echo "Use 'kubectl get ns' to make sure your pods are not in a Terminating status before redeploying"
