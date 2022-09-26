dafd60b7d9ca8c094274f7c3afd7731ec4644f22
mattburcke <mattburcke@microsoft.com>
2022-09-26 09:45:54 -0400

    commit by mattburcke

---
+++ 059-DatadogOnAzure/Student/Resources/Challenge-01/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    datadog = {
+      source = "DataDog/datadog"
+    }
+  }
+}
+
+# Configure the Datadog provider
+provider "datadog" {
+  api_key = var.datadog_api_key
+  app_key = var.datadog_app_key
+  api_url = "https://us3.datadoghq.com"
+}
