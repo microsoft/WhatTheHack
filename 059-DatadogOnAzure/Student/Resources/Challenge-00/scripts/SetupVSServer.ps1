01d9c3846a4bff8728abc7dba3deccebff217af9
Peter Laudati <plaudati@hotmail.com>
2022-10-02 13:12:28 -0400

    Added install of SQL Management Studio to the VS VM

--- 059-DatadogOnAzure/Student/Resources/Challenge-00/scripts/SetupVSServer.ps1
+++ 059-DatadogOnAzure/Student/Resources/Challenge-00/scripts/SetupVSServer.ps1
@@ -4,6 +4,32 @@ param (
     [string]$AdminUsername
 )
 
+# Install SQL Management Studio 18.1
+# Set file and folder path for SSMS installer .exe
+$folderpath="c:\windows\temp"
+$filepath="$folderpath\SSMS-Setup-ENU.exe"
+ 
+#If SSMS not present, download
+if (!(Test-Path $filepath)){
+write-host "Downloading SQL Server Latest SSMS..."
+$URL = "https://aka.ms/ssmsfullsetup"
+$clnt = New-Object System.Net.WebClient
+$clnt.DownloadFile($url,$filepath)
+Write-Host "SSMS installer download complete" -ForegroundColor Green
+ 
+}
+else {
+ 
+write-host "Located the SQL SSMS Installer binaries, moving on to install..."
+}
+ 
+# start the SSMS installer
+write-host "Beginning SSMS 2016 install..." -nonewline
+$Parms = " /Install /Quiet /Norestart /Logs log.txt"
+$Prms = $Parms.Split(" ")
+& "$filepath" $Prms | Out-Null
+Write-Host "SSMS installation complete" -ForegroundColor Green
+
 <#
 # Install Microsoft .Net Core 3.1.100
 $exeDotNetTemp = [System.IO.Path]::GetTempPath().ToString() + "dotnet-sdk-3.1.100-win-x64.exe"
