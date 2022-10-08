f7fe23a4976b0905b93fa2144f1f9a70679fb816
Peter Laudati <plaudati@hotmail.com>
2022-10-07 21:53:14 -0400

    Updated the SetupDatadogOnWebServers.ps1 file for Challenge 3 to have the installation code.  Action for students is to add their API key.

--- 059-DatadogOnAzure/Student/Resources/Challenge-03/SetupDatadogOnWebServers.ps1
+++ 059-DatadogOnAzure/Student/Resources/Challenge-03/SetupDatadogOnWebServers.ps1
@@ -2,5 +2,11 @@
 
 # Modify this script to add code that configures Datadog on the web servers running in the VM Scale Set
 
-# Create a folder to let us know the script is running.
-New-Item -Path 'C:\Datadog' -ItemType Directory
+# Download the Datadog agent
+$ProgressPreference = 'SilentlyContinue'
+wget "https://s3.amazonaws.com/ddagent-windows-stable/datadog-agent-7-latest.amd64.msi" -usebasicparsing -outfile .\ddagent.msi
+
+# Install Datadog using the downloaded MSI file
+# ACTION - Insert your own Datadog API Key
+Start-Process -Wait msiexec -ArgumentList '/qn /i ddagent.msi APIKEY="INSERT-YOUR-API-KEY-HERE" SITE="us3.datadoghq.com" TAGS="team:platform" DD_ENV=dev REBOOT=ReallySuppress ADDLOCAL="MainApplication,NPM" LOGS_ENABLED=true PROCESS_ENABLED=true APPSEC_ENABLED=true'
+$ProgressPreference = 'Continue'
