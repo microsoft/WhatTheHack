3dbbbcca9716f5d92bae9098414c76fa14b921b1
Peter C. Laudati <plaudati@hotmail.com>
2022-12-14 19:57:22 -0500

    Update and rename cliscript.sh to hacksetup.sh
    rename to hacksetup.sh and added prompts for user input when running.

---
+++ 019-ThisOldDataWarehouse/Student/Resources/Challenge0/hacksetup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Prompt user for resource group name & Azure region location:
+read -p "Enter a resource group name: " RGNAME
+read -p "Enter an Azure region to deploy to (ie. 'eastus', 'westus', 'northeurope'): " LOCATION
+
+#az login
+#az account set --subscription $subscription
+
+echo "Creating resource group '$RGNAME' in Azure region '$LOCATION'..."
+az group create --location $LOCATION --name $RGNAME
+
+echo "Deploying Azure Container Instance with source LOB databases..."
+az container create -g $RGNAME --name mdwhackdb --image whatthehackmsft/sqlserver2019_demo:1  --cpu 2 --memory 7 --ports 1433 --ip-address Public
+
+echo "Deploying Azure Data Factory, Azure SQL Server Instance & SSIS Runtime..."
+az deployment group create --name final --resource-group $RGNAME --template-file template.json --parameters parametersFile.json
