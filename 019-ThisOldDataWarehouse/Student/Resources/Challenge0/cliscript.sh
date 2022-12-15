b1bac7b91eafbf367050f648412aacb7f28d16f7
Peter C. Laudati <plaudati@hotmail.com>
2022-12-14 19:38:10 -0500

    Update cliscript.sh
    add parameters to the script.

--- 019-ThisOldDataWarehouse/Student/Resources/Challenge0/cliscript.sh
+++ 019-ThisOldDataWarehouse/Student/Resources/Challenge0/cliscript.sh
@@ -1,5 +1,15 @@
-az login
-az account set --subscription {ENTER SUBSCRIPTION NAME}
-az group create --location eastus2 --name {ENTER RESOURCE GROUP NAME}
-az container create -g {ENTER RESOURCE GROUP NAME} --name mdwhackdb --image whatthehackmsft/sqlserver2019_demo:1  --cpu 2 --memory 7 --ports 1433 --ip-address Public
-az deployment group create --name final --resource-group {ENTER RESOURCE GROUP NAME} --template-file template.json --parameters parametersFile.json
+#!/bin/bash
+
+#Prompt user for resource group name & Azure region location:
+read -p "Enter a resource group name where the hack's resources will be deployed. \n (If using a shared Azure subscription, include your initials in the resource group name): " RGNAME
+
+read -p "Enter an Azure region to deploy to (ie. 'eastus', 'westus', 'northeurope'): " LOCATION
+
+echo $RGNAME
+echo $LOCATION
+
+#az login
+#az account set --subscription $subscription
+#az group create --location $location --name $rgname
+#az container create -g $rgname --name mdwhackdb --image whatthehackmsft/sqlserver2019_demo:1  --cpu 2 --memory 7 --ports 1433 --ip-address Public
+#az deployment group create --name final --resource-group $rgname --template-file template.json --parameters parametersFile.json
