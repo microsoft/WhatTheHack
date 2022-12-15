62006e9ae4a0de09396683d9ec75c61bd460e307
Peter Laudati <plaudati@hotmail.com>
2022-12-15 13:30:37 -0500

    Modified script to accept password and resource prefix from the user.

--- 019-ThisOldDataWarehouse/Student/Resources/Challenge0/hacksetup.sh
+++ 019-ThisOldDataWarehouse/Student/Resources/Challenge0/hacksetup.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 #Prompt user for resource group name & Azure region location:
-read -p "Enter a resource group name: " RGNAME
+read -p "Enter a resource prefix name: " RESOURCEPREFIX
 read -p "Enter an Azure region to deploy to (i.e. 'eastus', 'westus', 'northeurope'): " LOCATION
+read -s -p "Enter a password for the SQL Server: " SQLPASSWORD
 
 #az login
 #az account set --subscription $subscription
+RGNAME="$RESOURCEPREFIX-rg"
+deploymentName="$RESOURCEPREFIX-deployment"
 
+echo
 echo "Creating resource group '$RGNAME' in Azure region '$LOCATION'..."
 az group create --location $LOCATION --name $RGNAME
 
@@ -14,4 +18,4 @@ echo "Deploying Azure Container Instance with source LOB databases..."
 az container create -g $RGNAME --name mdwhackdb --image whatthehackmsft/sqlserver2019_demo:1  --cpu 2 --memory 7 --ports 1433 --ip-address Public
 
 echo "Deploying Azure Data Factory, Azure SQL Server Instance & SSIS Runtime..."
-az deployment group create --name final --resource-group $RGNAME --template-file template.json --parameters parametersFile.json
+az deployment group create --name $deploymentName --resource-group $RGNAME --template-file deployHack.json --parameters @deployHackParameters.json --parameters databaseAdminPassword=$SQLPASSWORD
