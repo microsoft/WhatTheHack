47e181b90dd2b3c30e6bb6c7c6030905c57ae975
Jerry@MSFT <jerryemis@me.com>
2023-03-09 15:22:11 -0500

    Delete xxx-ChaosStudio4AKS directory
    removed old XXX-ChaosStudio4AKS

--- xxx-ChaosStudio4AKS/Coach/Solution-04.md
+++
@@ -1,5 +0,0 @@
-# Challenge 04 - Add Chaos to your pipeline - Coach's Guide 
-
-[< Previous Solution](./Solution-03.md) - **[Home](./README.md)**
-
-## Notes & Guidance
--- xxx-ChaosStudio4AKS/Student/Resources/EastUS-AKS/HelmCharts/ContosoPizza/pg.sql
+++
@@ -1,7 +0,0 @@
---Create the wth database
-CREATE DATABASE wth;
-
--- Create user contosoapp that would own the application schema
-
- CREATE ROLE CONTOSOAPP WITH LOGIN NOSUPERUSER INHERIT CREATEDB CREATEROLE NOREPLICATION PASSWORD 'OCPHack8';
-
