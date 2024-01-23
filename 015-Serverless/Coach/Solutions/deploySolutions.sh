065f929866fb2ddd818bd1eed4efd73f08ff7f33
Marc Garcia <marcgarcia@microsoft.com>
2024-01-23 11:09:47 -0500

    Fixed sequence of RBAC in Keyvault via CLI

--- 015-Serverless/Coach/Solutions/deploySolutions.sh
+++ 015-Serverless/Coach/Solutions/deploySolutions.sh
@@ -13,16 +13,12 @@ functionTollBoothEvents="wth-serverless-events"
 # Create a resource group
 az group create --name $RGName --location $location
 
-# Create an Azure Cosmos DB account
+# Create an Azure Cosmos DB account and a SQL-compatible Database
 az cosmosdb create --name $cosmosDbAccountName --kind GlobalDocumentDB --resource-group $RGName --locations regionName=$location 
-
-# Create a db
 az cosmosdb sql database create --account-name $cosmosDbAccountName --name LicensePlates --resource-group $RGName
 
-# Create a container inside the Cosmos DB account
+# Create the containers inside the Cosmos DB account
 az cosmosdb sql container create --account-name $cosmosDbAccountName --database-name LicensePlates --name Processed --partition-key-path /licensePlateText --resource-group $RGName
-
-# Create a similar second container
 az cosmosdb sql container create --account-name $cosmosDbAccountName --database-name LicensePlates --name NeedsManualReview --partition-key-path /fileName --resource-group $RGName
 
 # Create a storage account
@@ -39,31 +35,24 @@ az eventgrid topic create --name $eventGridTopicName --location $location --reso
 az cognitiveservices account create --name $computerVisionServiceName --kind ComputerVision --sku S1 --location $location --resource-group $RGName --yes
 
 # Create a Key Vault
-az keyvault create --name $keyVaultName --resource-group $RGName --location $location --sku standard --enable-rbac-authorization true
-
-
-# Get the Computer Vision API key
-computerVisionApiKey=$(az cognitiveservices account keys list --name $computerVisionServiceName --resource-group $RGName --query key1 -o tsv)
-
-# Get the Event Grid Topic Key
-eventGridTopicKey=$(az eventgrid topic key list --name $eventGridTopicName --resource-group $RGName --query key1 -o tsv)
-
-# Get the CosmosDB Authorization Key
-cosmosDBAuthorizationKey=$(az cosmosdb keys list --name $cosmosDbAccountName --resource-group $RGName --type keys --query primaryMasterKey -o tsv)
-
-# Get the blob Storage Connection string
-blobStorageConnection=$(az storage account show-connection-string --name $storageAccountName --resource-group $RGName --query connectionString -o tsv)
+az keyvault create --name $keyVaultName --resource-group $RGName --location $location --sku standard --enable-rbac-authorization false #we'll enable RBAC after creating the secrets via the CLI to avoid permission issues
 
 # Create secrets in the Key Vault
+computerVisionApiKey=$(az cognitiveservices account keys list --name $computerVisionServiceName --resource-group $RGName --query key1 -o tsv)
+eventGridTopicKey=$(az eventgrid topic key list --name $eventGridTopicName --resource-group $RGName --query key1 -o tsv)
+cosmosDBAuthorizationKey=$(az cosmosdb keys list --name $cosmosDbAccountName --resource-group $RGName --type keys --query primaryMasterKey -o tsv)
+blobStorageConnection=$(az storage account show-connection-string --name $storageAccountName --resource-group $RGName --query connectionString -o tsv)
+
 az keyvault secret set --vault-name $keyVaultName --name "computerVisionApiKey" --value $computerVisionApiKey
 az keyvault secret set --vault-name $keyVaultName --name "eventGridTopicKey" --value $eventGridTopicKey
 az keyvault secret set --vault-name $keyVaultName --name "cosmosDBAuthorizationKey" --value $cosmosDBAuthorizationKey
 az keyvault secret set --vault-name $keyVaultName --name "blobStorageConnection" --value $blobStorageConnection
 
-
+# Creat the function Apps
 az functionapp create --name $functionTollBoothApp --runtime dotnet --runtime-version 6 --storage-account $storageAccountName --consumption-plan-location "$location" --resource-group $RGName --functions-version 4
 az functionapp create --name $functionTollBoothEvents --runtime node --runtime-version 18 --storage-account $storageAccountName --consumption-plan-location "$location" --resource-group $RGName --functions-version 4
 
+# Grant permissions for the Functiosn to read secrets from KeyVault using RBAC
 keyvaultResourceId=$(az keyvault show --name $keyVaultName --resource-group $RGName -o tsv --query id) 
 
 ## ATTENTION ##
@@ -71,6 +60,10 @@ keyvaultResourceId=$(az keyvault show --name $keyVaultName --resource-group $RGN
 az functionapp identity assign -g $RGName -n $functionTollBoothApp --role "Key Vault Secrets User"   --scope $keyvaultResourceId
 az functionapp identity assign -g $RGName -n $functionTollBoothEvents  --role "Key Vault Secrets User"   --scope $keyvaultResourceId
 
+# Enable RBAC access policy in the KV, which will make our CLI permissions to the KV insufficient
+az keyvault update --name $keyVaultName --resource-group $resourceGroupName --enable-rbac-authorization true
+
+# Populate the Function App's AppSettings with the required values
 kvuri=$(az keyvault show  --name $keyVaultName --resource-group $RGName -o tsv --query properties.vaultUri) #returns URI, ends in /
 cognitiveEndpoint=$(az cognitiveservices account show --name $computerVisionServiceName -g $RGName  -o tsv --query properties.endpoint) #returns URI, ends in /
 eventgridEndpoint=$(az eventgrid topic show --name $eventGridTopicName --resource-group $RGName -o tsv --query endpoint) #doesn't end in /
