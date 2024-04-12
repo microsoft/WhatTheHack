a5d1301947ae987a831acb54dd03022f687e60c9
Saverio Proto <saverioproto@microsoft.com>
2024-04-12 14:28:09 +0200

    Refresh Terraform code to April 2024 provider and modules versions

--- 039-AKSEnterpriseGrade/Coach/Solutions/Challenge-02/Terraform/database.tf
+++ 039-AKSEnterpriseGrade/Coach/Solutions/Challenge-02/Terraform/database.tf
@@ -1,40 +1,33 @@
-resource "azurerm_mysql_server" "example" {
+resource "azurerm_mysql_flexible_server" "example" {
   name = random_string.random.result
   #location            = azurerm_resource_group.example.location
   location            = "eastus"
   resource_group_name = azurerm_resource_group.example.name
 
-  administrator_login          = "mysqlazureadmin"
-  administrator_login_password = var.databasepassword
+  administrator_login    = "mysqlazureadmin"
+  administrator_password = var.databasepassword
 
-  sku_name   = "GP_Gen5_2"
-  storage_mb = 5120
-  version    = "5.7"
+  sku_name = "GP_Standard_D2ds_v4"
 
-  auto_grow_enabled                 = true
-  backup_retention_days             = 7
-  geo_redundant_backup_enabled      = false
-  infrastructure_encryption_enabled = false
-  public_network_access_enabled     = false
-  ssl_enforcement_enabled           = false
+  backup_retention_days        = 7
+  geo_redundant_backup_enabled = false
 }
 
 resource "azurerm_private_endpoint" "example" {
   name                = "${random_string.random.result}-endpoint"
   location            = "westeurope"
   resource_group_name = azurerm_resource_group.example.name
-  subnet_id           = module.network.vnet_subnets[0]
+  subnet_id           = module.network.vnet_subnets_name_id["subnet1"]
 
   private_service_connection {
     name                           = "${random_string.random.result}-privateserviceconnection"
-    private_connection_resource_id = azurerm_mysql_server.example.id
+    private_connection_resource_id = azurerm_mysql_flexible_server.example.id
     subresource_names              = ["mysqlServer"]
     is_manual_connection           = false
   }
 }
 
 variable "databasepassword" {
-
   type      = string
   sensitive = true
 }
