461169a1f578c4b09f02bc68d72ab10e2adada34
Marc Garcia <1045276+marcosgm@users.noreply.github.com>
2024-10-30 11:07:10 -0400

    [Hack Update] 015-Serverless Update OCR API to v3.2 (#848)
    * [015-Serverless] express bicep template
    
    * [015-Serverless] Update API OCR to v3.2
    
    * typo
    
    * undo code changes
    
    * documented scripts Coach folder, msbuild lab, fix URL for OCR API
    
    * remove msbuild lab markdown, use txt
    
    * remove fake secrets to avoid security warnings
    
    * add spellchecks
    
    ---------
    
    Co-authored-by: Marc Garcia <marcgarcia@microsoft.com>

--- 015-Serverless/Coach/Solutions/azcli-challenge-01-to-06.sh.sh
+++ 015-Serverless/Coach/Solutions/azcli-challenge-01-to-06.sh.sh
@@ -76,7 +76,7 @@ eventgridEndpoint=$(az eventgrid topic show --name $eventGridTopicName --resourc
 cosmosDBEndpoint=$(az cosmosdb show  --name $cosmosDbAccountName --resource-group $RGName -o tsv --query documentEndpoint) #returns URI, ends in /
 
 #be careful, if running in WSL, it may pick up your windows AZ CLI instance and then append \r characters in the variable substitution. Ensure WSL has the interop setting to appendWindowsPath = false. It can also show up in the KV secrets, making .NET to complain "New-line characters are not allowed in header values."
-az functionapp config appsettings set -g $RGName -n $functionTollBoothApp --settings "computerVisionApiUrl="$cognitiveEndpoint"vision/v2.0/ocr"
+az functionapp config appsettings set -g $RGName -n $functionTollBoothApp --settings "computerVisionApiUrl="$cognitiveEndpoint"vision/v3.2/ocr"
 az functionapp config appsettings set -g $RGName -n $functionTollBoothApp --settings "computerVisionApiKey=@Microsoft.KeyVault(SecretUri="$kvuri"secrets/computerVisionApiKey/)"
 az functionapp config appsettings set -g $RGName -n $functionTollBoothApp --settings eventGridTopicEndpoint=$eventgridEndpoint
 az functionapp config appsettings set -g $RGName -n $functionTollBoothApp --settings "eventGridTopicKey=@Microsoft.KeyVault(SecretUri="$kvuri"secrets/eventGridTopicKey/)"
